5c8a0ce968ba1d99630dd1002ab9f09658d86247
Martin Schulte <abc@exampel.com>
2017-11-26 14:37:54 +0100

    first commit

---
+++ Window.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+/**
+ * Beschreiben Sie hier die Klasse Window.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Window extends Application
+{
+   // Attribute
+   public static Window firstWindow;
+   public static Window topWindow;
+   protected static int windowID;
+   private Color 
+
+    
+    
+    /**
+     * Konstruktor für Objekte der Klasse Window
+     */
+    public Window()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+   
+}
