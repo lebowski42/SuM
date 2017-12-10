1668d188279e72c551e034e6557810a75c417c2d
Martin Schulte <abc@exampel.com>
2017-12-10 20:16:25 +0100

    EventHandler

--- Window.java
+++ Window.java
@@ -12,7 +12,11 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.shape.*;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
-
+import javafx.scene.paint.Color;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.geometry.Insets;
 // JavaFX events
 import javafx.event.*;
 import javafx.scene.input.*;
@@ -32,8 +36,11 @@ public class Window
     public static Window firstWindow;
     public static Window topWindow;
     protected static int windowID;
-    private Scene scene;
+
     private JFrame jFrame ;
+
+    private Scene scene;
+    private Canvas canvas;
 
     private static String defaultFrameTitle = "SuM-Window";
 
@@ -45,9 +52,8 @@ public class Window
     private ArrayList<Character> keybuffer;
 
     private boolean useDoubleBuffering;
-    //private Color
-    
-    
+    private Color bgColor = Color.WHITE;
+
     KeyEvent e;
     String s;
 
@@ -148,7 +154,9 @@ public class Window
                 }
             });
         JFXPanel jFXPanel = new JFXPanel();
+        jFXPanel.setOpaque(true);
         jFrame.add(jFXPanel);
+
 
         Platform.runLater(new Runnable(){
                 @Override
@@ -162,10 +170,11 @@ public class Window
     private void initFx(JFXPanel pPanel){
         pPanel.setOpaque(false); //non Transparent
         Pane root = new Pane();
-        Canvas canvas = new Canvas(jFrame.getWidth(), jFrame.getHeight());
+        canvas = new Canvas(jFrame.getWidth(), jFrame.getHeight());
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.fillRoundRect(110, 60, 30, 30, 10, Math.random()*10);
-        Scene scene = new Scene(root, jFrame.getWidth(), jFrame.getHeight());
+        scene = new Scene(root, jFrame.getWidth(), jFrame.getHeight());
+        this.setColor(this.bgColor);
         // set JavaFX events
         // Mouse
         scene.setOnMousePressed(new EventHandler<MouseEvent>(){
@@ -176,7 +185,7 @@ public class Window
             });
         scene.setOnMouseReleased(new EventHandler<MouseEvent>(){
                 public void handle (MouseEvent mouseEvent) {
-                    setMousePosition(mouseEvent);
+                    setMousePosition(mouseEvent);   
                     isMouseButtonDown = false;
                     if(mouseEvent.getClickCount() > 1){
                         wasDoubleClick = true;
@@ -206,19 +215,15 @@ public class Window
         scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
                 public void handle(KeyEvent keyEvent){
                     s = keyEvent.getCharacter();
-                    //char c = (char) Integer.parseInt( s.substring(2), 16 );
+                    char c =  s.charAt(0);
                     e=keyEvent;
-                    System.out.println("Es wurde folgende Taste gedrückt:\t" + s);
-                }
+                    System.out.println("Es wurde folgende Taste gedrückt:\t" + Character.getNumericValue(c));                }
             });
         Circle circ = new Circle(40, 40, 30);
         Circle circ1 = new Circle(70, 50, 30);
-        ((Pane)scene.getRoot()).getChildren().add(circ);
+        //((Pane)scene.getRoot()).getChildren().add(circ);
         ((Pane)scene.getRoot()). getChildren().add(canvas);
-        ((Pane)scene.getRoot()).
-        getChildren().
-
-        add(circ1);
+        // ((Pane)scene.getRoot()).getChildren().add(circ1);
         pPanel.setScene(scene);
     }
 
@@ -250,6 +255,14 @@ public class Window
     //// set- and get-Methods /////
     ///////////////////////////////   
 
+    public int getHeight(){
+        return jFrame.getHeight();
+    }
+
+    public int getWidth(){
+        return jFrame.getWidth();
+    }
+
     protected double getMouseX(){
         return this.mouseX;
     }
@@ -258,12 +271,57 @@ public class Window
         return this.mouseY;
     }
 
-    public void setMousePosition(MouseEvent pMouseEvent){
+    protected void setMousePosition(MouseEvent pMouseEvent){
         this.mouseX = pMouseEvent.getX();
         this.mouseY = pMouseEvent.getY();
         this.buttonNumber = 1;
         if(pMouseEvent.getButton() == MouseButton.MIDDLE) this.buttonNumber = 2;
         if(pMouseEvent.getButton() == MouseButton.SECONDARY) this.buttonNumber = 3;
+    }
+
+    public void setColor(Color pColor){
+        canvas.setStyle("-fx-background-color: white;");
+        ((Pane)scene.getRoot()).setBackground(new Background(new BackgroundFill(pColor, CornerRadii.EMPTY, Insets.EMPTY)));
+        this.bgColor = pColor;
+    }
+
+    public Color getColor(){
+        return this.bgColor;
+    }
+
+    public void test(){
+        int a=100000;
+        while(a>0){
+            //canvas.getGraphicsContext2D().strokeLine(Math.random()*getWidth(), Math.random()*getHeight(), Math.random()*getWidth(), Math.random()*getHeight());
+            double x = Math.random()*getWidth();
+            double y = Math.random()*getHeight();
+            canvas.getGraphicsContext2D().strokeOval(x, y,20,20);
+            canvas.getGraphicsContext2D().setStroke(Color.WHITE);
+            canvas.getGraphicsContext2D().strokeOval(x, y,20,20);
+            canvas.getGraphicsContext2D().setStroke(Color.BLACK);
+
+            a--;
+        }
+    }
+    
+    public void test2(int p){
+        canvas.getGraphicsContext2D().setFontSmoothingType(null);
+        double lw = 1;
+        if(p==1){
+            canvas.getGraphicsContext2D().setStroke(Color.WHITE);
+            canvas.getGraphicsContext2D().setLineWidth(lw);
+        
+        }
+            
+        else  {
+            
+            canvas.getGraphicsContext2D().setStroke(Color.BLACK);
+            canvas.getGraphicsContext2D().setLineWidth(lw);
+        }
+
+        canvas.getGraphicsContext2D().strokeOval(50, 50,60,60);
+        //canvas.getGraphicsContext2D().strokeLine(20,20,100,100);
+        
     }
 
 }
