a2c77c411b12df8e74e5b5d0cb758f81259c0836
Martin Schulte <abc@exampel.com>
2017-12-14 21:35:30 +0100

    EventHandler

--- Pen.java
+++ Pen.java
@@ -9,37 +9,83 @@ import javafx.scene.paint.*;
 public class Pen
 {
     protected double x,y;
-    protected boolean penIsUp = true;
+    protected boolean isUp = true;
     protected double angel = 0;
-    
+
     protected Window myWindow;
-    
+    protected int currentNode;
+    protected Paint color=Color.BLACK;
+    protected double linewidth = 1;
+
+    protected static final int NORMALMODE = 0;
+    protected static final int ERASEMODE = 1;
+    protected static final int XORMODE = 2;
 
     /**
      * Konstruktor für Objekte der Klasse Pen
      */
     public Pen()
     {
-       x=0;
-       y=0;
-       myWindow = Window.firstWindow;
+        this(Window.topWindow);
     }
 
-    
+    public Pen(Window pWindow){
+        myWindow = pWindow;
+    }
+
     protected void drawLine(double x1, double y1, double x2, double y2) 
     {
         GraphicsContext g = myWindow.g();
         if (g != null)
-        {
-            System.out.println("Draw");
-           g.setStroke(Color.BLACK);
+        {          
             this.setState(g);       
             g.strokeLine(x1,y1,x2,y2);
         }
     }
-    
+
     protected void setState(GraphicsContext g){
-       // o
+        if(this.currentNode == NORMALMODE){
+            g.setStroke(this.color);
+            g.setLineWidth(this.linewidth);
+        }
+        if(this.currentNode == ERASEMODE){
+            g.setStroke(this.myWindow.getBgColor());
+            g.setLineWidth(this.linewidth*2.5);
+        }
+
+    }
+
+    public void normal() 
+    {
+        this.currentNode = this.NORMALMODE;
     }
     
+    public void erase() 
+    {
+        this.currentNode = this.ERASEMODE;
+    }
+
+    public void changeMode() 
+    {
+        this.currentNode = this.XORMODE;
+    }
+    
+    public double getX()
+    {
+        return this.x;
+    }
+    
+    public double getY(){
+        return this.y;
+    }
+
+    protected void setStandard(){
+        this.x = 0;
+        this.y = 0;
+        this.isUp = true;
+        this.angel = 0;
+        this.currentNode = this.NORMALMODE;
+        this.normal();
+    }
+
 }
